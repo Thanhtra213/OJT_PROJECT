abda5db47ec37d15627cc1ca23c31c44ed7cddc3
LOVBE <tranludangtrung@gmail.com>
2026-03-18 14:23:36 +0700

    Upload data cho course, teacher

---
+++ EasyEnglish_API/DTOs/Upload/FileUploadRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyEnglish_API.DTOs.Upload
+{
+    public class FileUploadRequest
+    {
+        public IFormFile File { get; set; } = null!;
+        public string Type { get; set; } = null!;
+    }
+}
---
+++ EasyEnglish_API/Services/Upload/IUpLoadService.cs
@@ -0,0 +1,9 @@
+﻿using EasyEnglish_API.DTOs.Upload;
+
+namespace EasyEnglish_API.Services.Upload
+{
+    public interface IUpLoadService
+    {
+        Task<string> UploadAssetAsync(FileUploadRequest req);
+    }
+}
